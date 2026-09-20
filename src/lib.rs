bab8adad9afa81ab0874815b51b60a9c04dc784d
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-20 23:37:11 +0800

    feat: export persistent volume binding API

--- src/lib.rs
+++ src/lib.rs
@@ -35,8 +35,9 @@ pub use cow_diff_apply::{
     CowDiffApplyError, CowDiffApplyLimits, CowDiffApplyReport,
 };
 pub use policy::{
-    ExecutableNeededBinding, PolicyError, ResourceLimits, SandboxPolicy, SeccompArgRangeRule,
-    SeccompArgRule, SeccompPolicy, StdioMode, StdioPolicy, MAX_EXECUTABLE_NEEDED_BINDINGS,
+    ExecutableNeededBinding, PersistentVolumeBinding, PolicyError, ResourceLimits, SandboxPolicy,
+    SeccompArgRangeRule, SeccompArgRule, SeccompPolicy, StdioMode, StdioPolicy,
+    MAX_EXECUTABLE_NEEDED_BINDINGS, MAX_PERSISTENT_VOLUME_BINDINGS,
 };
 pub use report::{
     CapturedOutput, ChildOutcome, CowDiff, CowDiffEntry, EnforcementReceipt, ProcessTreeUsage,

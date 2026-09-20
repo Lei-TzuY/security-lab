38619cbb9c4c33b5ccc659722863a1de65cea2c2
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-21 05:32:58 +0800

    feat: export host UNIX seqpacket grants

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,8 @@ pub use report::{
 };
 pub use runtime_fd_broker::{
     HostUnixPeerCredentials, HostUnixRouteGrant, HostUnixStreamRevocationController,
-    PreparedHostUnixStream, PreparedReadOnlyRegularFile, PreparedRevocableByteStream,
+    PreparedHostUnixSeqpacket, PreparedHostUnixStream, PreparedReadOnlyRegularFile,
+    PreparedRevocableByteStream,
     PreparedRuntimeMessageChannel, PreparedSealedRegularFileSnapshot, PreparedSealedSnapshotBundle,
     RevocableByteStreamController, RuntimeAcknowledgedCorrelatedMessageExchangeController,
     RuntimeAuthenticatedCorrelatedMessageExchangeController,
